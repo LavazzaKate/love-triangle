c4c069c1767bb2fbfbd26dc1359283953179e31c
LavazzaKate <nebo10@mail.ru>
2018-09-13 22:27:51 +0300

    second

--- src/index.js
+++ src/index.js
@@ -4,4 +4,17 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
-};
+let count=0;
+for (let i=0; i<preferences.length; i++){
+
+  let firstSpichonees = preferences[i];
+  let secondSpichonees = preferences[[i]-1];
+  let thirdSpichonees = preferences[[[i]-1]-1];
+
+  if (firstSpichonees != secondSpichonees && secondSpichonees!=thirdSpichonees && thirdSpichonees==(i+1) ){
+
+count++
+  }
+}
+return count/3;
+} ;
